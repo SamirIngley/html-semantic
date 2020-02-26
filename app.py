3afdc1adb060f8c2e8b24f63b012544c7b0465a4
SamirIngley <samir.ingle7@gmail.com>
2020-02-25 23:39:41 -0800

    flask running

--- app.py
+++ app.py
@@ -5,16 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+    return render_template('index3.html')
