93738c476c211f010e440f1e4677540d100c7189
SamirIngley <samir.ingle7@gmail.com>
2020-02-28 17:51:55 -0800

    cleaned up

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask import render_template
+from livereload import Server
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index3.html')
+
+if __name__ == '__main__':
+    server = Server(app.wsgi_app)
+    server.serve()
