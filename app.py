a9514b91b352730f940eccf8fe57d6bbf3e65a55
SamirIngley <samir.ingle7@gmail.com>
2020-03-03 17:28:54 -0800

    nav fixes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import render_template
-from livereload import Server
 
 
 app = Flask(__name__)
@@ -8,7 +7,3 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index3.html')
-
-if __name__ == '__main__':
-    server = Server(app.wsgi_app)
-    server.serve()
